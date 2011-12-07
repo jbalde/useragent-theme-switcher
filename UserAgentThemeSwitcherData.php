7e46ce25fc1f67e6651ee2a480e4c200bbe853b5
Juan Benavides Romero <jbalde@gmail.com>
2011-12-07 13:12:25 +0100

    Change browsers list from new static autogenerated class BrowserList

--- UserAgentThemeSwitcherData.php
+++ UserAgentThemeSwitcherData.php
@@ -283,59 +283,7 @@ class UserAgentThemeSwitcherData {
 	 * Generate all supported browsers
 	 */
 	private function generateBrowsers() {
-		$this->browsers = array();
-
-		$this->addBrowser(new BrowserUA('googlebot', 'GoogleBot', null, '^Mozilla\/5\.0 \(compatible; Googlebot\/.\..; \+http:\/\/www\.google\.com\/bot\.html\)$', 'google'));
-		$this->addBrowser(new BrowserUA('ie2', 'Internet Explorer 2', null, '^Mozilla\/1\.22 \(compatible; MSIE 2\.0;.*\).*$', 'ie'));
-		$this->addBrowser(new BrowserUA('ie5', 'Internet Explorer 5', null, '^Mozilla\/4\.0 \(compatible; MSIE 5\.0.*\).*$', 'ie'));
-		$this->addBrowser(new BrowserUA('ie55', 'Internet Explorer 5.5', null, '^Mozilla\/4\.0 \(compatible; MSIE 5\.5;.*\).*$', 'ie'));
-		$this->addBrowser(new BrowserUA('ie6', 'Internet Explorer 6', null, '^Mozilla\/4\.0 \(compatible; MSIE 6\.0;.*\).*$', 'ie'));
-		$this->addBrowser(new BrowserUA('ie7', 'Internet Explorer 7', null, '^Mozilla\/4\.0 \(compatible; MSIE 7\.0; Windows NT.*\).*$', 'ie'));
-		$this->addBrowser(new BrowserUA('ie8', 'Internet Explorer 8', null, '^Mozilla\/4\.0 \(compatible; MSIE 8\.0;.*\).*$', 'ie'));
-		$this->addBrowser(new BrowserUA('ie9', 'Internet Explorer 9', null, '^Mozilla\/5\.0 \(compatible; MSIE 9\.0;.*\).*$', 'ie'));
-		$this->addBrowser(new BrowserUA('chrome', 'Google Chrome', null, '^Mozilla\/5.0 \(.*\) AppleWebKit\/.* \(KHTML, like Gecko\).* Chrome\/.* Safari\/.*$', 'webkit'));
-		$this->addBrowser(new BrowserUA('safaridesktop', 'Safari', null, '^Mozilla\/5\.0 \(.*; .*\) AppleWebKit\/.* \(KHTML, like Gecko\) Version\/[\d\.]+ Safari\/.*$', 'webkit'));
-		$this->addBrowser(new BrowserUA('firefox', 'Firefox', null, '^Mozilla\/5\.0 \(.*\) Gecko\/.* Firefox\/.*$', 'gecko'));
-		$this->addBrowser(new BrowserUA('operadesktop', 'Opera', null, '^Opera\/[\d\.]+( ){0,1}\(.*\).*$', 'opera'));
-		$this->addBrowser(new BrowserUA('safarimobile', 'Safari Mobile', null, '^Mozilla\/5.0 \(.*\) AppleWebKit\/.* \(KHTML, like Gecko\)( Version\/.*){0,1} Mobile[\/A-Z0-9]{0,}( Safari\/.*){0,1}$', 'webkit,mobile'));
-		$this->addBrowser(new BrowserUA('operamini', 'Opera Mini', null, '^Opera\/.* \(.*Opera Mini\/.*\).*$', 'opera,mobile'));
-		$this->addBrowser(new BrowserUA('camino', 'Camino', null, '^Mozilla\/5.0 \(.*\) Gecko\/[\d]+ Camino\/[\d\.]+ \(like Firefox\/[\d\.]+\)$', 'gecko'));
-
-		$this->addBrowser(new BrowserUA('iceweasel', 'IceWeasel', null, '^Mozilla\/5.0 \(.*\) Gecko\/[\d]+ Iceweasel\/[\d\.]+ \(Debian-.*\).*$', 'gecko'));
-		$this->addBrowser(new BrowserUA('rockmelt', 'RockMelt', null, '^Mozilla\/5.0 \(Macintosh; .*\) AppleWebKit\/534\.24 \(KHTML, like Gecko\) RockMelt\/.* Chrome\/.* Safari\/.*$', 'webkit'));
-
-		$this->addBrowser(new BrowserUA('ipad', 'iPad', null, '^Mozilla\/5.0 \(iPad; .*\).*$', 'tablet'));
-		$this->addBrowser(new BrowserUA('kindle', 'Amazon Kindle', null, '^Mozilla\/5\.0 \(.*\) AppleWebKit\/.* \(KHTML, like Gecko(.*) Version\/[\d\.]+ Kindle\/.*$', 'tablet'));
-		$this->addBrowser(new BrowserUA('ie7mobile', 'Internet Explorer Mobile 7', null, '^Mozilla\/4\.0 \(compatible; MSIE 7\.0; Windows Phone OS.*\).*$', 'ie,mobile'));
-		$this->addBrowser(new BrowserUA('firefoxmobile', 'Firefox Mobile', null, '^Mozilla\/5\.0 \(Android.*\) Gecko\/.* Firefox\/.*$', 'gecko,mobile'));
-
-		$this->addBrowser(new BrowserUA('googlebotmobile', 'GoogleBot Mobile', null, '\(compatible; Googlebot-Mobile\/.\..; \+http:\/\/www\.google.com\/bot.html\)$', 'google,mobile'));
-
-		$this->addBrowser(new BrowserUA('wordpressandroid', 'Wordpress Android', null, '^wp-android\/.*$', 'spider'));
-		$this->addBrowser(new BrowserUA('wordpressiphone', 'Wordpress iPhone', null, '^wp-iphone\/.*$', 'spider'));
-		$this->addBrowser(new BrowserUA('wordpressipad', 'Wordpress iPad', null, '^WordPress .* \(iPad; iPhone OS .*\)$', 'spider'));
-		$this->addBrowser(new BrowserUA('wordpressweb', 'Wordpress Web', null, '^WordPress\/.*$', 'spider'));
-
-		$this->addBrowser(new BrowserUA('java', 'Java', null, '^Java\/.*$', 'spider'));
-		$this->addBrowser(new BrowserUA('bitacoras', 'Bitacoras', null, '^Bitacoras.com\/2\.0 \(http:\/\/bitacoras\.com\)$', 'spider'));
-		$this->addBrowser(new BrowserUA('flash', 'Flash', null, '^Shockwave Flash$', 'spider'));
-		$this->addBrowser(new BrowserUA('zendcrawler', 'Zend Crawler', null, '^Zend_Http_Client$', 'spider'));
-		$this->addBrowser(new BrowserUA('wget', 'wget', null, '^Wget.*$', 'spider'));
-		$this->addBrowser(new BrowserUA('powermarks', 'Powermarks', null, '^Mozilla\/4\.0 \(compatible; Powermarks/.*$', 'spider'));
-		$this->addBrowser(new BrowserUA('applepubsub', 'Apple-PubSub', null, '^Apple\-PubSub\/.*$', 'spider'));
-		$this->addBrowser(new BrowserUA('alexa', 'Alexa', null, '^ia_archiver \(\+http:\/\/www\.alexa\.com\/site\/help\/webmasters; crawler@alexa\.com\)$', 'spider'));
-		$this->addBrowser(new BrowserUA('libperl', 'libperl', null, '^libwww-perl\/.*$', 'spider'));
-		$this->addBrowser(new BrowserUA('bitly', 'bit.ly', null, '^bitlybot$', 'spider'));
-		$this->addBrowser(new BrowserUA('twitter', 'Twitter Bot', null, '^Twitterbot\/.*$', 'spider'));
-		$this->addBrowser(new BrowserUA('slurp', 'Yahoo Slurp', null, '^Mozilla\/5\.0 \(compatible; Yahoo\! Slurp; http:\/\/help\.yahoo\.com\/help\/us\/ysearch\/slurp\)$', 'spider'));
-		$this->addBrowser(new BrowserUA('yahoocachesystem', 'Yahoo Cache System', null, '^YahooCacheSystem$', 'spider'));
-		$this->addBrowser(new BrowserUA('blackberry', 'Blackberry Browser', null, '^BlackBerry.* Profile\/MIDP-.* Configuration\/CLDC-.* VendorID\/.*$', 'mobile'));
-		$this->addBrowser(new BrowserUA('msnbot', 'MSNBot', null, '^msnbot-UDiscovery\/2\.0b \( http:\/\/search\.msn\.com\/msnbot\.htm\)$', 'crawler'));
-
-
-		$this->addBrowser(new BrowserUA('facebook', 'Facebook', null, '^facebookexternalhit\/1\.1 \( http:\/\/www\.facebook.com\/externalhit_uatext\.php\)$', 'crawler'));
-		$this->addBrowser(new BrowserUA('moreoverbot', 'Moreoverbot', null, '^Moreoverbot\/5\.1 \( http:\/\/w\.moreover\.com; webmaster@moreover\.com\) Mozilla\/5\.0$', 'crawler'));
-
+		$this->browsers = BrowserList::getBrowsers();
 
 		$configuratedTemplates = $this->getConfiguratedTemplates();
 		$countConfiguredBrowsers = count($configuratedTemplates);
